8e22e91e93ca41c4a2f1801f0433ddb7b4020720
wespy01 <bhardwajansh0911@gmail.com>
2024-06-14 16:23:20 +0530

    Update script.js

--- 8 Working with API/script.js
+++ 8 Working with API/script.js
@@ -1,32 +1,24 @@
-const button = document.querySelector('button')
-const image = document.querySelectorAll('img')
 
-// -------------------------------------------------------Using fetch to interact with the API---------------------------------------------------------------------
-
-// --------------------------When there was only one image to be shown------------------------------------------------
-// const button = document.querySelectorAll('button')
+// when there is only image _______________________________________________
+// const button = document.querySelector('button')
 // const image = document.querySelector('img')
-
 // button.addEventListener('click', () => {
-//     fetch('https://dog.ceo/api/breeds/image/random')
-//     .then(response => response.json())
-//     .then(json => console.log(json.message))
-//     image.src = json.message
-// })
 
-
-
-
-// button.addEventListener('click', () => {
 //     fetch('https://dog.ceo/api/breeds/image/random')
 //         .then(response => response.json())
 //         .then(json => {
-//             console.log(json.message)
 //             image.src = json.message
 //         })
 // })
 
-// --------------------------When multiple images were to be shown, forEach was used------------------------------------------------
+
+
+
+// when there are multiple images _________________________________________
+const button = document.querySelector('button')
+const image = document.querySelectorAll('img')
+
+// we use forEach with fetch here _________________________________________
 // image.forEach(image => {
 //     button.addEventListener('click', () => {
 //         fetch('https://dog.ceo/api/breeds/image/random')
@@ -39,29 +31,17 @@ const image = document.querySelectorAll('img')
 // })
 
 
-// ------------------------------------------------USING XHR REQUEST NOW----------------------------------------------------------- 
 
 
 
+// we use forEach with XML HTTP Request here_______________________________
 image.forEach(images => {
     button.addEventListener('click', () => {
         const xhr = new XMLHttpRequest()
-        // console.log(xhr);
-
-// addEventListener should be used isntead of onload-------------------------------------------------------------------
-
         xhr.addEventListener('load', () => {
             const response = JSON.parse(xhr.response);
             images.src = response.message;
         })
-
-// Below onload code is also working but addeventlistener is preferred------------------------------------------------
-
-        // xhr.onload = () => {
-        //     const respo = JSON.parse(xhr.response);
-        //     images.src = respo.message;
-        // }
-
         xhr.open('GET', 'https://dog.ceo/api/breeds/image/random')
         xhr.send()
     })
